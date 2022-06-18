262d5a63dba98ad1693224b51d7566fa36ffd423
yingchengpeng <yingchengpeng@youzan.com>
2022-06-18 22:46:55 +0800

    添加新的文档和版本

---
+++ mall-user/mall-user-biz/src/main/java/com/yingchengpeng/mall/user/biz/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 应用服务
+ * @author yingchengpeng
+ * @date 2022/6/18 9:24 PM
+ */
+package com.yingchengpeng.mall.user.biz;
---
+++ mall-user/mall-user-gateway-rpc/src/main/java/com/yingchengpeng/mall/user/gateway/rpc/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * rpc服务
+ * @author yingchengpeng
+ * @date 2022/6/18 9:22 PM
+ */
+package com.yingchengpeng.mall.user.gateway.rpc;
